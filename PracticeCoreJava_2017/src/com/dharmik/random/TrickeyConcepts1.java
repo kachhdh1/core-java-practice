6c2fdd465e285341840fc0d54098b8ed8711883e
kachhdh1 <dharmik.kachhia@yahoo.com>
2017-07-21 09:36:07 +0530

    algo and java concepts

---
+++ PracticeCoreJava_2017/src/com/dharmik/random/TrickeyConcepts1.java
@@ -0,0 +1,13 @@
+package com.dharmik.random;
+
+public class TrickeyConcepts1 {
+
+	public static void main(String[] args) {
+		/*
+		 * this will return 0
+		 */
+		System.out.println(Math.min(Double.MIN_VALUE, 0.0d));
+		
+	}
+
+}
