ea137ca4ab907d0c1685e8a1b3ff7aef0ac189ea
Dharmik Kachhia <dharmik.kachhia@citiustech.com>
2019-10-04 18:54:50 +0530

    functional techniques avoiding nulls (before)

---
+++ JavaExamples/src/main/java/com/dk/functionaltechniques/avoidingnull/Customer.java
@@ -0,0 +1,13 @@
+package com.dk.functionaltechniques.avoidingnull;
+
+public class Customer {
+    private RewardPoints rewardPoints;
+
+    public void setRewardPoints(RewardPoints rewardPoints) {
+        this.rewardPoints = rewardPoints;
+    }
+
+    public RewardPoints getRewardPoints() {
+        return rewardPoints;
+    }
+}
---
+++ JavaExamples/src/main/java/com/dk/functionaltechniques/avoidingnull/Order.java
@@ -0,0 +1,9 @@
+package com.dk.functionaltechniques.avoidingnull;
+
+import java.math.BigDecimal;
+
+public class Order {
+    public BigDecimal getTotal() {
+        return BigDecimal.TEN;
+    }
+}
