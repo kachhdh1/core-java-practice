f6844ea78a8e9bff2252a6acbea11c89f090eb80
Dharmik Kachhia <dharmik.kachhia@citiustech.com>
2019-10-05 18:18:11 +0530

    Avoiding nulls, DiscountService example cleanup

--- JavaExamples/src/main/java/com/dk/functionaltechniques/avoidingnull/Order.java
+++
@@ -1,9 +0,0 @@
-package com.dk.functionaltechniques.avoidingnull;
-
-import java.math.BigDecimal;
-
-public class Order {
-    public BigDecimal getTotal() {
-        return BigDecimal.TEN;
-    }
-}
