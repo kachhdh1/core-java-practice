71d73aea24357486ebee16a26e993a78b344a046
Dharmik Kachhia <dharmik.kachhia@citiustech.com>
2020-01-06 16:57:29 +0530

    Simple dynamic bean creation and invoke method

--- JavaExamples/src/main/java/com/dk/beancreater/SimpleBeanCreator.java
+++ JavaExamples/src/main/java/com/dk/beancreater/SimpleBeanCreator.java
@@ -1,23 +1,26 @@
 package com.dk.beancreater;
 
-import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
+import java.util.Arrays;
+import java.util.List;
 
 import net.sf.cglib.beans.BeanGenerator;
 
 public class SimpleBeanCreator {
-	
-	public static void main( String[] args ) throws IllegalAccessException, IllegalArgumentException, InvocationTargetException, NoSuchMethodException, SecurityException
-    {
-    	BeanGenerator beanGenerator = new BeanGenerator();
-    	 
-    	beanGenerator.addProperty("name", String.class);
-    	Object myBean = beanGenerator.create();
-    	Method setter = myBean.getClass().getMethod("setName", String.class);
-    	setter.invoke(myBean, "some string value set by a cglib");
-    	 
-    	Method getter = myBean.getClass().getMethod("getName");
-    	System.out.println(getter.invoke(myBean));
-    }
+
+	public static void main(String[] args) throws Exception {
+		List<String> argument_list = Arrays.asList("name", "age", "salary");
+		
+		BeanGenerator beanGenerator = new BeanGenerator();
+
+		argument_list.forEach(arg -> beanGenerator.addProperty(arg, String.class));
+		
+		Object myBean = beanGenerator.create();
+		Method setter = myBean.getClass().getMethod("setName", String.class);
+		setter.invoke(myBean, "Dharmik");
+
+		Method getter = myBean.getClass().getMethod("getName");
+		System.out.println(getter.invoke(myBean));
+	}
 
 }
